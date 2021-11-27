5eeb5da584486cd3f632bf8c209dbb694258d38e
luzikato <anasnurf99@gmail.com>
2021-11-27 12:55:24 +0700

    Added saving mechanism with JSON

--- main.py
+++ main.py
@@ -38,8 +38,15 @@ def save():
     if len(website) == 0 or len(password) == 0:
         messagebox.showwarning(title="Oops", message="Please make sure you haven't left any fields empty.")
     else:
+        with open('data.json', 'r') as data_file:
+            # Reading old data
+            data = json.load(data_file)
+            # Updating old data with new data
+            data.update(new_data)
+
         with open('data.json', 'w') as data_file:
-            json.dump(new_data, data_file)
+            # Saving updated data
+            json.dump(data, data_file, indent=4)
             website_entry.delete(0, END)
             password_entry.delete(0, END)
 # ---------------------------- UI SETUP ------------------------------- #
